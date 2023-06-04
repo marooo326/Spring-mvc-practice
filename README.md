25686f4d59e056a5ed935a5c276fc7f1b61feb6b
marooo326 <ktnqkr01@gmail.com>
2023-06-04 21:55:25 +0900

    2. 서블릿

---
+++ README.md
@@ -0,0 +1,2 @@
+# Spring-mvc-practice
+- [서블릿](./정리/2.%20서블릿.md)
---
+++ src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -0,0 +1,31 @@
+package hello.servlet.basic.response;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.security.Principal;
+
+@WebServlet(name = "responseHeaderServlet", urlPatterns = "/response-header")
+public class ResponseHeaderServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setStatus(HttpServletResponse.SC_FOUND);
+
+        response.setHeader("Content-Type", "text/plain");
+        response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        response.setHeader("Pragma", "no-cache");
+        response.setHeader("my-header", "hello");
+
+        PrintWriter writer = response.getWriter();
+        writer.println("ok");
+
+        // redirect
+        response.setHeader("Location", "/basic/hello-form.html");
+
+    }
+}
